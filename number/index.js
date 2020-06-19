282d1eacb3b872f0017caeb6a73622eecae724e0
vvdolphin <1273027520@qq.com>
2020-06-19 16:10:56 +0800

    first

---
+++ number/index.js
@@ -0,0 +1,8 @@
+for(var i=1;i<=20;i++){
+    if (i%2 == 1){
+        document.write(i+" is odd number.");
+    }else{
+        document.write((i+" is even number."));
+    }
+    document.write("<br/>");
+}
